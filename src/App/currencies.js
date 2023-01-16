d40166c823fd2535483655faa3ae9c4ccd9ab9e0
Ihor Domnich <mr.igordomnich@gmail.com>
2023-01-16 17:20:39 +0100

    added currenvies

---
+++ src/App/currencies.js
@@ -0,0 +1,12 @@
+export const currencies = [
+    {
+        short: "USD",
+        name: "American Dollar",
+        rate: 4.33,
+    },
+    {
+        short: "EUR",
+        name: "Euro",
+        rate: 4.69,
+    },
+];

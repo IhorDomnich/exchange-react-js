ac2c0e12b7af42250bdf1b12224ecb133e037501
Ihor Domnich <mr.igordomnich@gmail.com>
2023-01-16 17:24:09 +0100

    transferring the application to react

---
+++ src/App/Form/index.js
@@ -0,0 +1,64 @@
+import React, { useState } from "react";
+import { currencies } from "../currencies";
+import { Result } from "./Result";
+import "./style.css";
+
+export const Form = ({ calculateResult, result }) => {
+    const [currency, setCurrency] = useState(currencies[0].short);
+    const [amount, setAmount] = useState("");
+
+    const onSubmit = (event) => {
+        event.preventDefault();
+        calculateResult(currency, amount);
+    }
+
+    return (
+        <form className="form" onSubmit={onSubmit}>
+            <h1 className="form__header">
+                Exchange
+            </h1>
+            <p>
+                <label>
+                    <span className="form__lableText">
+                        Enter the amount PLN*:
+                    </span>
+                    <input
+                        value={amount}
+                        onChange={({ target }) => setAmount(target.value)}
+                        placeholder=" Enter the amount PLN"
+                        className="form__field"
+                        type="number"
+                        required
+                        step="0.01"
+                    />
+                </label>
+            </p>
+            <p>
+                <label>
+                    <span className="form__lableText">
+                        Currency
+                    </span>
+                    <select
+                        className="form__field"
+                        value={currency}
+                        onChange={({ target }) => setCurrency(target.value)}
+                    >
+                        {currencies.map((currency => (
+                            <option
+                                key={currency.short}
+                                value={currency.short}
+                            >
+                                {currency.name}
+                            </option>
+                        )))}
+                        </select>
+                </label>
+            </p>
+            <p>
+                <button className="form__button">Calculate!</button>
+            </p>
+
+            <Result result={result} />
+        </form>
+    );
+};
